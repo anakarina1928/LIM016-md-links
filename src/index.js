a8bc02dee6377660ee78e395ed98dade47438a0f
anakarina1928 <anakarina.nak@gmail.com>
2022-01-04 09:24:41 -0500

    unificacion de los objetos que estan en ambos arreglos

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
-const api = require('./api.js"');
-/*
-const mdLinks = () => {
-    const arrayLinks = readingLinks('./data/data.md');
-    //console.log(arrayLinks);
-    const promiseStatusLinks = makeHttpRequest(arrayLinks);
-  
-    return promiseStatusLinks.then( statusHttpArray => {
-      return arrayLinks.map( (element, index) => {
-        return {
-          ...element,
-          status: statusHttpArray[index]
-        }
+const api = require('./api');
+const userPath = './data/data.md';
+
+/*La función debe retornar una promesa (Promise) que resuelva a 
+un arreglo (Array) de objetos (Object) ¡se debe unificar los dos!*/ 
+
+const mdLinks = ( userPath, options) => {
+  return new Promise((resolve, reject) => {
+
+    if (options.validate === true) {
+      
+      const arrayLinks = api.readingLinks(userPath);//array de objetos
+      const promiseStatusLinks = api.makeHttpRequest(arrayLinks);//array de objetos que contienen la forma: { status: 200, }
+      
+      //Cuando se resuelva la promesa "promiseStatusLinks", es cuando vamos a resolver la promesa que estamos creando
+      promiseStatusLinks.then( arrayHttpStatus => { //Aqui adentro hacemos la unificacion de los objetos que estan en ambos arreglos
+        let unificationOfObjects = arrayLinks.map( (element, index) => {
+          return {
+            ...element,
+            ...arrayHttpStatus[index]
+          }
+        })
+        resolve(unificationOfObjects)
       })
-    });
-  };
-  
-  mdLinks().then(finalArray => console.log(finalArray));*/
+       
+    }
+  });
+}
+
+  mdLinks(userPath, { validate: true}).then(finalArray => console.log('final array: ', finalArray));
   //console.log("Promesa resuelta");
